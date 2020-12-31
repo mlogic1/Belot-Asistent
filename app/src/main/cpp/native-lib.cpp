3bb7045c4420ab061b9991084759c2c304911034
mlogic1 <multilogic@outlook.com>
2020-12-31 11:18:39 +0100

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,19 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
+#include <BeloteController.h>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_merodyadt_belotasistent_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+Java_com_merodyadt_belotasistent_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */)
+{
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_merodyadt_belotasistent_MainActivity_InitBeloteController(JNIEnv* env, jobject /* this */)
+{
+    std::string hello = "Hello from C++";
+	Belote::BeloteController* cntrl = new Belote::BeloteController();
+    std::cout<<hello<<std::endl;
 }
--- app/src/main/java/com/merodyadt/belotasistent/MainActivity.java
+++ app/src/main/java/com/merodyadt/belotasistent/MainActivity.java
@@ -1,18 +1,21 @@
 package com.merodyadt.belotasistent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
     // Used to load the 'native-lib' library on application startup.
     static
     {
         System.loadLibrary("native-lib");
     }
+
+    private Toolbar m_toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,14 +23,22 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        m_toolBar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(m_toolBar);
+
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        InitBeloteController();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
     public native String stringFromJNI();
+    public native void InitBeloteController();
 }
